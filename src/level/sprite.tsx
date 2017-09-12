f044f833476fb9fd80d3e658cab66d38d86011f0
Julien-laville <alternative451@myopera.com>
2017-09-12 21:05:46 +0200

    reclean

---
+++ src/level/sprite.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="sprite" tilewidth="32" tileheight="32" tilecount="32" columns="8">
+ <image source="../../sprite.png" width="258" height="130"/>
+</tileset>
